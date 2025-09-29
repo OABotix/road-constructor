607a5f1a982702d31e7356f810a84b5cf64358cb
Olwethu Alexandra Bhiya <ST10497311@rcconnect.edu.za>
2025-09-29 14:07:27 +0200

    first commit

---
+++ src/main/java/com/olwethuab/roadconstructionreport/RoadConstructionReport.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.olwethuab.roadconstructionreport;
+
+/**
+ *
+ * @author oabhi
+ */
+public class RoadConstructionReport {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
